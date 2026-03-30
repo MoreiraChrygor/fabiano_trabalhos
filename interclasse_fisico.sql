1a45a4f9a44b6fcfe0f6d303e011a930588efc65
MoreiraChrygor <conta.github@outlook.com>
2026-03-30 12:25:12 -0300

    Add files via upload

---
+++ interclasse_fisico.sql
@@ -0,0 +1,82 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `interclasse` DEFAULT CHARACTER SET utf8 ;
+USE `interclasse` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`tbl_modalidade`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `interclasse`.`tbl_modalidade` (
+  `id_modalidade` INT NOT NULL,
+  `modalidade` VARCHAR(60) NOT NULL,
+  PRIMARY KEY (`id_modalidade`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`tbl_times`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `interclasse`.`tbl_times` (
+  `idl_times` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  `tbl_modalidade_id_modalidade` INT NOT NULL,
+  PRIMARY KEY (`idl_times`),
+  INDEX `fk_tbl_times_tbl_modalidade1_idx` (`tbl_modalidade_id_modalidade` ASC) VISIBLE,
+  CONSTRAINT `fk_tbl_times_tbl_modalidade1`
+    FOREIGN KEY (`tbl_modalidade_id_modalidade`)
+    REFERENCES `mydb`.`tbl_modalidade` (`id_modalidade`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`tbl_jogadores`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `interclasse`.`tbl_jogadores` (
+  `id_jogadores` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  `tbl_times_idl_times` INT NOT NULL,
+  PRIMARY KEY (`id_jogadores`),
+  INDEX `fk_tbl_jogadores_tbl_times1_idx` (`tbl_times_idl_times` ASC) VISIBLE,
+  CONSTRAINT `fk_tbl_jogadores_tbl_times1`
+    FOREIGN KEY (`tbl_times_idl_times`)
+    REFERENCES `interclasse`.`tbl_times` (`idl_times`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`boletim`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `interclasse`.`boletim` (
+  `id_boletim` INT NOT NULL AUTO_INCREMENT,
+  `frequencia` TINYINT NOT NULL,
+  `mencao` TINYINT NOT NULL,
+  `ocorrencia` VARCHAR(45) NOT NULL,
+  `tbl_jogadores_id_jogadores` INT NOT NULL,
+  PRIMARY KEY (`id_boletim`),
+  INDEX `fk_boletim_tbl_jogadores1_idx` (`tbl_jogadores_id_jogadores` ASC) VISIBLE,
+  CONSTRAINT `fk_boletim_tbl_jogadores1`
+    FOREIGN KEY (`tbl_jogadores_id_jogadores`)
+    REFERENCES `interclasse`.`tbl_jogadores` (`id_jogadores`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
